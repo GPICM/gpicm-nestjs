f137b2105705a2f42ef777652b65fa80465e4a2c
fonsecaBarreto <lucasbfonte@gmail.com>
2025-05-01 15:42:22 -0300

    feat: add default Throttle to application

--- src/modules/reports/partner-metrics.controller.ts
+++ src/modules/reports/partner-metrics.controller.ts
@@ -9,6 +9,7 @@ import {
 import { CacheInterceptor } from "@nestjs/cache-manager";
 import { MongoDbStationDailyMetricsRepository } from "./infra/repositories/mongodb/mongodb-stations-daily-metrics-repository";
 import { PartnerApiKeyGuard } from "../identity/presentation/meta/guards/partner-api-key.guard";
+import { Throttle } from "@nestjs/throttler";
 
 @Controller("partners/metrics")
 @UseGuards(PartnerApiKeyGuard)
@@ -20,6 +21,7 @@ export class PartnerMetricsController {
 
   @Get("/:stationSlug")
   @UseInterceptors(CacheInterceptor)
+  @Throttle({ default: { limit: 6, ttl: 60000 } })
   findMetricsByStations(@Param("stationSlug") stationSlug: string): any {
     const nowStr = new Date().toISOString();
     return this.mongoDbStationDailyMetricsRepository.getLatestMetricsByStation(
