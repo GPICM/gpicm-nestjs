eb7e942a6e2b9a9efbd25b5cff4d9ea2f2c9857b
fonsecaBarreto <lucasbfonte@gmail.com>
2025-05-16 21:04:15 -0300

    refactor: update MongoRegionDailyRanking

--- database/mongodb/schemas/region-daily-rankings.ts
+++ database/mongodb/schemas/region-daily-rankings.ts
@@ -1,15 +1,35 @@
-import { Db, ObjectId } from "mongodb";
+import { ObjectId } from "mongodb";
 
 export const REGION_DAILY_RANKINGS_COLLECTION_NAME = "region_daily_rankings";
+
+export type RankingSummary = {
+  stationSlug: string;
+  value: number;
+  timestamp: Date;
+};
 
 export interface MongoRegionDailyRanking {
   _id: ObjectId;
   date: Date;
 
-  byMaxTemperature: Array<{ stationSlug: string; value: number }>;
-  byMinTemperature: Array<{ stationSlug: string; value: number }>;
-  byRainVolumeAcc: Array<{ stationSlug: string; value: number }>;
-  byMaxWindSpeed: Array<{ stationSlug: string; value: number }>;
+  // Temperature
+  byLatestTemperature: Array<RankingSummary>;
+  byMaxTemperature: Array<RankingSummary>;
+  byMinTemperature: Array<RankingSummary>;
+
+  // Rain Volume
+  byRainVolumeAcc: Array<RankingSummary>;
+
+  // WindSpeed
+  byMaxWindSpeed: Array<RankingSummary>;
+  byLatestWindSpeed: Array<RankingSummary>;
+  byLatestWindDirection: Array<RankingSummary>;
+
+  // Air humidity
+  byLatestAirHumidity: Array<RankingSummary>;
+
+  // Atm
+  byLatestAtmosphericPressure: Array<RankingSummary>;
 
   updatedAt: Date;
 }
