0aa6c122a8342ab7df3b1a18fbfca744a41a062c
fonsecaBarreto <lucasbfonte@gmail.com>
2025-06-06 21:21:22 -0300

    feat: add post migration

---
+++ database/mysql/prisma/migrations/20250607002107_refactor_posts/migration.sql
@@ -0,0 +1,51 @@
+-- AlterTable
+ALTER TABLE `posts` ADD COLUMN `down_votes` INTEGER NOT NULL DEFAULT 0,
+    ADD COLUMN `is_pinned` BOOLEAN NOT NULL DEFAULT false,
+    ADD COLUMN `is_verified` BOOLEAN NOT NULL DEFAULT false,
+    ADD COLUMN `score` INTEGER NOT NULL DEFAULT 0,
+    ADD COLUMN `up_votes` INTEGER NOT NULL DEFAULT 0;
+
+-- CreateTable
+CREATE TABLE `post_medias` (
+    `mediaId` INTEGER NOT NULL,
+    `postId` INTEGER NOT NULL,
+    `order` INTEGER NULL,
+
+    PRIMARY KEY (`mediaId`, `postId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `medias` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `url` VARCHAR(191) NOT NULL,
+    `filename` VARCHAR(191) NOT NULL,
+    `contentType` VARCHAR(191) NOT NULL,
+    `size` INTEGER NULL,
+    `type` ENUM('IMAGE', 'AUDIO', 'VIDEO', 'DOCUMENT') NOT NULL,
+    `provider` ENUM('LOCAL', 'S3', 'OTHER') NOT NULL DEFAULT 'LOCAL',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `post_votes` (
+    `post_id` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `value` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`post_id`, `user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `post_medias` ADD CONSTRAINT `post_medias_mediaId_fkey` FOREIGN KEY (`mediaId`) REFERENCES `medias`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `post_medias` ADD CONSTRAINT `post_medias_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `posts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `post_votes` ADD CONSTRAINT `post_votes_post_id_fkey` FOREIGN KEY (`post_id`) REFERENCES `posts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `post_votes` ADD CONSTRAINT `post_votes_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
