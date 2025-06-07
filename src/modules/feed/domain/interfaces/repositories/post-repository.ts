0b0f0f572afb16dcff4b36263f14ec1663971ebc
fonsecaBarreto <lucasbfonte@gmail.com>
2025-06-06 21:33:59 -0300

    refactor: PostRepository

---
+++ src/modules/feed/domain/interfaces/repositories/post-repository.ts
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+
+import { Post } from "../../entities/Post";
+
+export abstract class PostRepository {
+  abstract add(
+    post: Post,
+    options?: { transactionContext?: unknown }
+  ): Promise<void>;
+
+  abstract update(
+    post: Post,
+    options?: { transactionContext?: unknown }
+  ): Promise<void>;
+
+  abstract findBySlug(slug: string): Promise<Post | null>;
+
+  abstract listAll(
+    filters: BaseRepositoryFindManyFilters
+  ): Promise<BaseRepositoryFindManyResult<Post>>;
+}
+
+// TODO: MOVE TO SOMEWHERE ELSE
+export interface BaseRepositoryFindManyFilters {
+  search?: string;
+  offset?: number;
+  limit?: number;
+  sort?: string;
+  order?: "asc" | "desc";
+}
+
+export interface BaseRepositoryFindManyResult<Model> {
+  count: number;
+  records: Model[];
+}
