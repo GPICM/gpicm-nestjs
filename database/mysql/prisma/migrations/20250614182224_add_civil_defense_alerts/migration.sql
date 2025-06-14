266c9cee7348741b22013ffb734a334c1c026408
fonsecaBarreto <lucasbfonte@gmail.com>
2025-06-14 15:23:55 -0300

    refactor: add long text to civil defenses title and description

--- database/mysql/prisma/migrations/20250614182224_add_civil_defense_alerts/migration.sql
+++ database/mysql/prisma/migrations/20250614182224_add_civil_defense_alerts/migration.sql
@@ -1,9 +1,10 @@
 -- CreateTable
 CREATE TABLE `civil_defense_alerts` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
+    `title` LONGTEXT NOT NULL,
+    `description` LONGTEXT NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
     `gravity_level` ENUM('LOW', 'MEDIUM', 'HIGH', 'VERY_HIGH', 'EXTREME') NOT NULL,
     `status` ENUM('ACTIVE', 'INACTIVE') NOT NULL,
 
