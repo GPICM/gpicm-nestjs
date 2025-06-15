de8c5d097927a7bac5c5806c5115ee1a14d3539d
fonsecaBarreto <lucasbfonte@gmail.com>
2025-06-15 01:01:12 -0300

    refactor: AssetsController

--- src/modules/assets/presentation/controllers/assets.controller.ts
+++ src/modules/assets/presentation/controllers/assets.controller.ts
@@ -21,11 +21,13 @@ export class AssetsController {
     private readonly blobRepository: BlobStorageRepository
   ) {}
 
-  @Get(":fileName")
+  @Get("*filePath")
   public async getOne(
-    @Param("fileName") fileName: string,
+    @Param("filePath") fullPath: string,
     @Res() res: Response
   ) {
+    const fileName = fullPath.split(",").join("/");
+
     this.logger.log(`Fetching file with name: ${fileName}`);
 
     const fileStream = await this.blobRepository.stream(fileName);
