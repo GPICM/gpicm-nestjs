92c9e41ebc8f202211b2d6ec53eb5753f7a10fec
fonsecaBarreto <lucasbfonte@gmail.com>
2025-06-29 11:46:02 -0300

    refactor: PostComment entity

--- src/modules/feed/domain/entities/PostComment.ts
+++ src/modules/feed/domain/entities/PostComment.ts
@@ -1,4 +1,4 @@
-import { User } from "@/modules/identity/domain/entities/User";
+import { UserShallow } from "./UserShallow";
 
 export enum CommentType {
   COMMENT = "COMMENT",
@@ -14,34 +14,34 @@ export type PartialUserForComment = {
 
 export class PostComment {
   public readonly id: number | null;
+
   public readonly postId: number;
-  public readonly userId: number;
+
   public readonly content: string;
+
   public readonly createdAt: Date;
+
   public readonly updatedAt: Date;
-  public readonly user: PartialUserForComment | null;
+
+  public readonly user: UserShallow;
+
   public readonly parentCommentId?: number | null;
 
   constructor(args: {
     id: number | null;
     postId: number;
-    userId: number;
     content: string;
-    parentCommentId?: number | null;
-    parentComment?: PostComment | null;
-    parentCommentType?: CommentType;
+    user: UserShallow;
     createdAt: Date;
     updatedAt: Date;
-    user: PartialUserForComment | null;
-    replyTo?: PostComment | null;
+    parentCommentId?: number | null;
   }) {
     this.id = args.id ?? null;
     this.postId = args.postId;
-    this.userId = args.userId;
     this.content = args.content;
+    this.user = args.user;
     this.createdAt = args.createdAt;
     this.updatedAt = args.updatedAt;
-    this.user = args.user;
     this.parentCommentId = args.parentCommentId ?? null;
   }
 
@@ -57,10 +57,7 @@ export class PostComment {
     return {
       id: this.id,
       postId: this.postId,
-      userId: this.userId,
       content: this.content,
-      createdAt: this.createdAt,
-      updatedAt: this.updatedAt,
       user: this.user,
       parentCommentId: this.parentCommentId ?? null,
       type: this.type,
