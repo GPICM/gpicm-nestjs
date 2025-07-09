b470957b68c0dba6efd49c9966e5a7b4da474f7f
fonsecaBarreto <lucasbfonte@gmail.com>
2025-07-09 13:21:41 -0300

    feat: add MongoStationDailyPrecipitationMetrics

---
+++ database/mongodb/schemas/station-daily-precipitation-metrics.ts
@@ -0,0 +1,25 @@
+import { ObjectId } from 'mongodb';
+
+export const STATION_DAILY_PRECIPITATION_METRICS_COLLECTION_NAME =
+  'station_daily_precipitation_metrics';
+
+export interface MongoStationDailyPrecipitationMetrics {
+  _id: ObjectId;
+  date: Date;
+
+  // Station
+  stationSlug: string;
+  stationDescription: string;
+  stationAddress: string;
+
+  // Precipitation
+  latestRainVolume: number | null;
+  rainVolumeAcc: number | null;
+  rainVolumeAccPerHour: number[];
+
+  // Metadata
+  isOnline: boolean;
+  recordsCount: number;
+  lastRecordAt: Date;
+  updatedAt: Date;
+}
