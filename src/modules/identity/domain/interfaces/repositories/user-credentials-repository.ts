4c6fef81f39873b8146e1615ca76bd7c6cf23a5a
fonsecaBarreto <lucasbfonte@gmail.com>
2025-07-14 22:25:16 -0300

    feat: new methods to UserCredentialsRepository

--- src/modules/identity/domain/interfaces/repositories/user-credentials-repository.ts
+++ src/modules/identity/domain/interfaces/repositories/user-credentials-repository.ts
@@ -1,5 +1,11 @@
 import { UserCredential } from "../../../authentication/domain/entities/UserCredential";
+import { AuthProviders } from "../../enums/auth-provider";
 
 export abstract class UserCredentialsRepository {
   abstract add(user: UserCredential, tx?: unknown): Promise<void>;
+  abstract update(user: UserCredential, tx?: unknown): Promise<void>;
+  abstract findOne(
+    filters: { userId: number; provider: AuthProviders },
+    tx?: unknown
+  ): Promise<void>;
 }
