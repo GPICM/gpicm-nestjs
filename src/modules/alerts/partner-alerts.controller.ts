e1810ce025b31ce148a9398905436bdaf96a5c75
Lucas Fonseca Barreto <47372714+fonsecaBarreto@users.noreply.github.com>
2025-08-26 21:57:35 -0300

    Alerts v2 (#51)
    * feat: add new attributes to civil defense alerts
    
    * chore: add zod
    
    * feat: add TelemetriaGpicmAlertsRepository
    
    * fix: CivilDefenseAlert
    
    * feat: implement alerts webhook
    
    * feat: implement plain text Description

--- src/modules/alerts/partner-alerts.controller.ts
+++ src/modules/alerts/partner-alerts.controller.ts
@@ -8,15 +8,40 @@ import {
 } from "@nestjs/common";
 import { Throttle } from "@nestjs/throttler";
 import { PartnerApiKeyGuard } from "../identity/presentation/meta/guards/partner-api-key.guard";
+import { CivilDefenseAlertsRepository } from "./domain/interfaces/alerts-repository";
+import { PrismaService } from "../shared/services/prisma-services";
+import { TelemetriaGpicmAlertsRepository } from "./infra/repositories/telemetria-gpicm-alerts-repository";
 
 @Controller("alerts/webhook")
 @UseGuards(PartnerApiKeyGuard)
 export class PartnerAlertsController {
-  constructor() {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly telemetriaGpicmAlertsRepository: TelemetriaGpicmAlertsRepository,
+    private readonly civilDefenseAlertsRepository: CivilDefenseAlertsRepository,
+
+  ) {}
 
   @Post("/")
   @Throttle({ default: { limit: 6, ttl: 60000 } })
-  public webhook(): Promise<any> {
+  public async webhook(): Promise<any> {
+
+    const newAlerts = await this.telemetriaGpicmAlertsRepository.listAll();
+
+    await this.prismaService.openTransaction(async (txContext) => {
+      await this.civilDefenseAlertsRepository.inactiveAll({ txContext });
+
+      if (newAlerts.length) {
+        await Promise.all(
+          newAlerts.map(async (newAlert) => {
+            await this.civilDefenseAlertsRepository.upsert(newAlert, {
+              txContext,
+            });
+          })
+        );
+      }
+    });
+
     return Promise.resolve({ status: "Recebido" });
   }
 
