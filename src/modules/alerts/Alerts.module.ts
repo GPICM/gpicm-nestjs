5039a725c8c4df844b4446c84d81aeabc708e580
fonsecaBarreto <lucasbfonte@gmail.com>
2025-09-01 08:37:29 -0300

    fix: remove unused  repository

--- src/modules/alerts/Alerts.module.ts
+++ src/modules/alerts/Alerts.module.ts
@@ -5,14 +5,12 @@ import { PrismaCivilDefenseAlertsRepository } from "./infra/repositories/prisma-
 import { PrismaService } from "../shared/services/prisma-services";
 import { PartnerAlertsController } from "./partner-alerts.controller";
 import { TelemetriaGpicmAlertsRepository } from "./infra/repositories/telemetria-gpicm-alerts-repository";
-import { MariaDbCivilDefenseAlertsRepository } from "./infra/repositories/mariadb-telemetria-gpicm-alerts-repository";
 
 @Module({
   controllers: [CivilDefenseAlertsController, PartnerAlertsController],
   providers: [
     PrismaService,
     TelemetriaGpicmAlertsRepository,
-    MariaDbCivilDefenseAlertsRepository,
     {
       provide: CivilDefenseAlertsRepository,
       useClass: PrismaCivilDefenseAlertsRepository,
