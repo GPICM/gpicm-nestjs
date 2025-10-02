534efa443e25f8f7b3eca013464749aebaf3fa3b
fonsecaBarreto <lucas.fonseca.ext@bemfacil.com.br>
2025-10-02 09:39:59 -0300

    feat: add FindProfileByUserUseCase

---
+++ src/modules/social/core/application/find-profile-by-user.usecase.ts
@@ -0,0 +1,18 @@
+import { Profile } from "@/modules/social/core/domain/entities/Profile";
+import { Inject, Injectable } from "@nestjs/common";
+import { ProfileRepository } from "../domain/interfaces/repositories/profile-repository";
+
+import { User } from "@/modules/identity/domain/entities/User";
+
+@Injectable()
+export class FindProfileByUserUseCase {
+  constructor(
+    @Inject(ProfileRepository)
+    private readonly profileRepository: ProfileRepository
+  ) {}
+
+  async execute(user: User): Promise<Profile | null> {
+    const profile = await this.profileRepository.findByUserId(user.id);
+    return profile;
+  }
+}
--- src/modules/social/core/external-profile.module.ts
+++ src/modules/social/core/external-profile.module.ts
@@ -3,17 +3,19 @@ import { PrismaProfileRepository } from "@/modules/social/core/infra/repositorie
 import { ProfileRepository } from "@/modules/social/core/domain/interfaces/repositories/profile-repository";
 
 import { CreateProfileUseCase } from "./application/create-profile.usecase";
+import { FindProfileByUserUseCase } from "./application/find-profile-by-user.usecase";
 
 @Module({
   imports: [],
   controllers: [],
   providers: [
     CreateProfileUseCase,
+    FindProfileByUserUseCase,
     {
       provide: ProfileRepository,
       useClass: PrismaProfileRepository,
     },
   ],
-  exports: [CreateProfileUseCase],
+  exports: [CreateProfileUseCase, FindProfileByUserUseCase],
 })
 export class ExternalProfileModule {}
