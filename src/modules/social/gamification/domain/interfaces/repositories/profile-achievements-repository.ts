f7f2d176c79e24125a4ac933cbf2f5cd7c2e38aa
fonsecaBarreto <lucas.fonseca.ext@bemfacil.com.br>
2025-10-13 17:56:50 -0300

    feat: (ProfileAchievementRepository) add listAll method

--- src/modules/social/gamification/domain/interfaces/repositories/profile-achievements-repository.ts
+++ src/modules/social/gamification/domain/interfaces/repositories/profile-achievements-repository.ts
@@ -1,4 +1,13 @@
+import {
+  BaseRepositoryFindManyFilters,
+  BaseRepositoryFindManyResult,
+} from "@/modules/social/core/domain/interfaces";
 import { ProfileAchievement } from "@/modules/social/gamification/domain/entities/ProfileAchievement";
+
+export interface AchievementsFindManyFilters
+  extends BaseRepositoryFindManyFilters {
+  profileHandle?: number;
+}
 
 export abstract class ProfileAchievementRepository {
   abstract add(
@@ -7,4 +16,8 @@ export abstract class ProfileAchievementRepository {
   ): Promise<void>;
   abstract findByProfile(profileId: number): Promise<ProfileAchievement[]>;
   abstract exists(profileId: number, achievementId: number): Promise<boolean>;
+
+  abstract listAll(
+    filters?: AchievementsFindManyFilters
+  ): Promise<BaseRepositoryFindManyResult<ProfileAchievement>>;
 }
--- src/modules/social/gamification/infra/repositores/prisma-profile-achievement-repository.ts
+++ src/modules/social/gamification/infra/repositores/prisma-profile-achievement-repository.ts
@@ -2,8 +2,13 @@ import { Injectable, Logger } from "@nestjs/common";
 import { PrismaService } from "@/modules/shared/services/prisma-services";
 
 import { PrismaProfileAchievementAssembler } from "./mappers/prisma-profile-achievement.assembler";
-import { ProfileAchievementRepository } from "../../domain/interfaces/repositories/profile-achievements-repository";
+import {
+  AchievementsFindManyFilters,
+  ProfileAchievementRepository,
+} from "../../domain/interfaces/repositories/profile-achievements-repository";
 import { ProfileAchievement } from "../../domain/entities/ProfileAchievement";
+import { BaseRepositoryFindManyResult } from "@/modules/social/core/domain/interfaces";
+import { Prisma } from "@prisma/client";
 
 @Injectable()
 export class PrismaProfileAchievementRepository
@@ -56,6 +61,58 @@ export class PrismaProfileAchievementRepository
         `Failed to check existence of achievement ${achievementId} for profile ${profileId}`,
         { error }
       );
+      throw error;
+    }
+  }
+
+  public async listAll(
+    filters?: AchievementsFindManyFilters
+  ): Promise<BaseRepositoryFindManyResult<ProfileAchievement>> {
+    try {
+      this.logger.log("Listing all achievements");
+      const { limit, offset, profileHandle } = filters || {};
+
+      const where: Prisma.ProfileAchievementWhereInput = { };
+
+      if (profileHandle) {
+        where.Profile = {
+          handle: profileHandle,
+        };
+      }
+
+      const [count, data] = await Promise.all([
+        this.prisma.profileAchievement.count({
+          take: limit,
+          skip: offset,
+          where,
+        }),
+        this.prisma.profileAchievement.findMany({
+          take: limit,
+          skip: offset,
+          where,
+        }),
+      ]);
+
+      const records: ProfileAchievement[] = [];
+      if (data.length) {
+        for (const ach of data) {
+          try {
+            this.logger.log("Parsing profile achievement", { ach });
+            const parsed = PrismaProfileAchievementAssembler.fromPrisma(ach);
+            if (parsed) records.push(parsed);
+          } catch (parseError: unknown) {
+            this.logger.warn("Failed to parse individual achievement", {
+              parseError,
+              ach,
+            });
+            continue;
+          }
+        }
+      }
+
+      return { count, records };
+    } catch (error: unknown) {
+      this.logger.error("Failed to List all achievements", { error });
       throw error;
     }
   }
